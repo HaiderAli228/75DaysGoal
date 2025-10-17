2e95eaaac5147db1803a4d0279466807cfd361b3
HaiderAli228 <flutter2830@gmail.com>
2025-10-17 23:56:03 +0500

    adding the Day 1 work : Login page with gradient bg

--- lib/Day1/login_page.dart
+++ lib/Day1/login_page.dart
@@ -8,6 +8,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,40 +25,73 @@ class _LoginPageState extends State<LoginPage> {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Email',
-              hintText: 'example@gmai.com',
-              border: OutlineInputBorder(),
-            ),
-          ),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Password',
-              border: OutlineInputBorder(),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 20),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              gradient: LinearGradient(
-                colors: [Colors.blue, Colors.purple],
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'example@gmai.com',
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-            child: ElevatedButton(
-              onPressed: () {
-                // Handle login action
-              },
-              child: Text('Login'),
-            ),
+              SizedBox(height: 20),
+              TextFormField(
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  if (_formKey.currentState!.validate()) {
+                    debugPrint("Process Data Tap");
+                  }
+                  debugPrint("Login Tap");
+                },
+                child: Container(
+                  margin: EdgeInsets.only(top: 20),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    gradient: LinearGradient(
+                      colors: [Colors.blue, Colors.purple],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                    ),
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
