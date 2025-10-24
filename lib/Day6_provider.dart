4097771bd6d38991c353eb0b80be3bc4f926a386
HaiderAli228 <flutter2830@gmail.com>
2025-10-24 23:20:17 +0500

    learning the Provider state managment

---
+++ lib/Day6_provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Day6Provider extends StatelessWidget {
+  const Day6Provider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
