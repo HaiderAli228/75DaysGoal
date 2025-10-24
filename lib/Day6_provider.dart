7e4ef5b9d494f1fa02a7f23e20b98a4f036972f9
HaiderAli228 <flutter2830@gmail.com>
2025-10-24 23:44:37 +0500

    learning the Provider state managment

--- lib/Day6_provider.dart
+++ lib/Day6_provider.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+
 class Day6Provider extends StatelessWidget {
   const Day6Provider({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Provider State management"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [],
+        ),
+      ),
+    );
   }
 }
